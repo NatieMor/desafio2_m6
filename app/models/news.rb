8dd25f242bb1e3cb80c5a3d22adfd1dde596a226
Nathalie Moraga <129231844+NatieMor@users.noreply.github.com>
2023-07-20 11:11:25 -0400

    Update news.rb

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,5 @@
 class News < ApplicationRecord
-    has_many :comments 
+    has_many :comments, dependent: :destroy
+    accepts_nested_attributes_for :comments, allow_destroy: true
     belongs_to :user
 end
